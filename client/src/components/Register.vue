aafd58ffda7b59312ddaac66e666ccd874f66e08
dinesh <dineshpanda92@gmail.com>
2018-07-10 09:57:01 +0530

    Add vuetify

---
+++ client/src/components/Register.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container fluid>
+    <v-layout column>
+      <v-flex xs12>
+        <v-form>
+          <v-text-field
+            v-model="email"
+            label="E-mail"
+            required>
+            </v-text-field>
+        </v-form>
+        </v-form>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: ''
+    }
+  }
+}
+</script>
+
+<style>
+  @import '~vuetify/src/stylus/main'
+</style>
+
