418f5aa2a45098331bac636b90fcc217dfcb70a2
dinesh <dineshpanda92@gmail.com>
2018-07-11 09:55:59 +0530

    client register poc

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,31 +1,44 @@
 <template>
-  <v-container fluid>
-    <v-layout column>
-      <v-flex xs12>
-        <v-form>
-          <v-text-field
-            v-model="email"
-            label="E-mail"
-            required>
-            </v-text-field>
-        </v-form>
-        </v-form>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <div id="app">
+    <v-app>
+      <v-form ref="form">
+        <v-text-field
+          v-model="email"
+          label="E-mail"
+          required
+        ></v-text-field>
+        <v-text-field
+            v-model="password"
+            :type="show1 ? 'text' : 'password'"
+            label="Password"
+            hint="At least 8 characters"
+            counter
+          ></v-text-field>
+        <v-btn @click.prevent="submit">submit</v-btn>
+    </v-form>
+  </v-app>
+</div>
 </template>
 
 <script>
+import AuthenticationService from '@/services/AuthenticationService'
+
 export default {
-  data() {
+  data () {
     return {
-      email: ''
+      email: '',
+      password: '',
+      show1: false
+    }
+  },
+  methods: {
+    async submit () {
+      const res = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(res.data);
     }
   }
 }
 </script>
-
-<style>
-  @import '~vuetify/src/stylus/main'
-</style>
-
